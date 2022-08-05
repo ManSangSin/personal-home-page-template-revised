f0ea5e8b2a9ab39dbb2b888a7b9a9ad6f78b66fd
Man Sang Sin <man.sang.sin@gmail.com>
2022-08-05 19:41:18 +0100

    added font file. cleaned and edited style.css

--- src/script.js
+++ src/script.js
@@ -1,12 +1,32 @@
+var sheep_counter, other_counter;
 
-var fruits, item;
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
 
 
-fruits = ['Dragonfruit', 'Mango', 'Lychee', 'Cherrie'];
-let element_list = document.getElementById('list');
-fruits.forEach((item) => {
-  let new_li = document.createElement('li');
-  new_li.innerText = item;
+let element_sheep_count = document.getElementById('sheep_count');
+element_sheep_count.innerText = 0;
+let element_other_count = document.getElementById('other_count');
+element_other_count.innerText = 0;
 
-  element_list.appendChild(new_li);
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_text = document.getElementById('text');
+  if (getNumberOrString(document.getElementById('text').value) == 'sheep') {
+    sheep_counter = (typeof sheep_counter === 'number' ? sheep_counter : 0) + 1;
+    let element_sheep_count2 = document.getElementById('sheep_count');
+    element_sheep_count2.innerText = sheep_counter;
+  } else {
+    other_counter = (typeof other_counter === 'number' ? other_counter : 0) + 1;
+    let element_other_count2 = document.getElementById('other_count');
+    element_other_count2.innerText = other_counter;
+  }
+
 });
